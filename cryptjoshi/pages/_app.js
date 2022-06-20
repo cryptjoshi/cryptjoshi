1ae3262ffc700675f7f5bc92791097ad8f715775
pornchai khamrat <itplayground@hotmail.com>
2022-06-20 19:19:23 +0700

    update

--- cryptjoshi/pages/_app.js
+++ cryptjoshi/pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-
+// import '../styles/globals.css'
+import Head from 'next/head'
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import customTheme from '../theme'
+import Header from '../components/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider theme={customTheme}>
+        <Head>
+          <meta
+            content='width=device-width,initial-scale=1' name='viewport' />
+          <meta name='theme-color' content='#2BB0EC' />
+        </Head>
+        <Header />
+        <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
