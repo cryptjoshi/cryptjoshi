bce16e765b1f8cf5e6edb8bf79d0754a7cdac5de
pornchai khamrat <itplayground@hotmail.com>
2022-06-22 19:43:02 +0700

    update header footer

--- cryptjoshi/pages/index.js
+++ cryptjoshi/pages/index.js
@@ -1,16 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box, SimpleGrid, VStack } from '@chakra-ui/react'
+import Section from "./../components/section"
+import Card from "./../components/mycard"
 const Home = () => {
   return (
     <Box as="h1">
+      <Section>
+        <VStack spacing={8}>
+          <SimpleGrid column={1} spacing={4} mt={8} w="100%">
+            <Card title="Hello" desc="iamdeveloperfromearth816" />
+          </SimpleGrid>
+        </VStack>
+      </Section>
     </Box>
   )
 }
 
-export async function getStaticProps(context) {
-  return {
-    props: {},
-    revalidate: 600
-  }
-}
 
 export default Home
